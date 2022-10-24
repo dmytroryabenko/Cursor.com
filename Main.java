d74bc2a9526b7a3ee3bd115046f296404f71d033
dmytroryabenko <116278537+dmytroryabenko@users.noreply.github.com>
2022-10-24 14:36:25 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,15 +10,11 @@ public class Main {
 
         System.out.println(c);
 
-
-
         short d = 20;
         short e = 4;
         int f = d - e;
 
         System.out.println(f);
-
-
 
         float h = 20;
         float l = 3;
@@ -26,25 +22,16 @@ public class Main {
 
         System.out.println(k);
 
-
-
         int m = 100;
         int n = 200;
         long p = m*n;
 
         System.out.println(p);
 
-
-
         int r = 15;
         int q = 8;
         int s = r % q;
 
         System.out.println(s);
-
-
-
-
-
     }
 }
